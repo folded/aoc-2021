6b74d2594efcd9ff2ad433fd5d77ccbb36c6e9a7
tobiasjs <tobiasjs@google.com>
2021-12-08 09:55:45 +0000

    Day 8 optimization and reformatting

--- 08/08-2.py
+++ 08/08-2.py
@@ -1,21 +1,22 @@
 import sys
 
 canonical = {
-  1: frozenset('cf'),
-  7: frozenset('acf'),
-  4: frozenset('bcdf'),
-  2: frozenset('acdeg'),
-  3: frozenset('acdfg'),
-  5: frozenset('abdfg'),
-  0: frozenset('abcefg'),
-  6: frozenset('abdefg'),
-  9: frozenset('abcdfg'),
-  8: frozenset('abcdefg'),
+    1: frozenset('cf'),
+    7: frozenset('acf'),
+    4: frozenset('bcdf'),
+    2: frozenset('acdeg'),
+    3: frozenset('acdfg'),
+    5: frozenset('abdfg'),
+    0: frozenset('abcefg'),
+    6: frozenset('abdefg'),
+    9: frozenset('abcdfg'),
+    8: frozenset('abcdefg'),
 }
 
-canonical_inv = { v: k for k,v in canonical.items() }
+canonical_inv = {v: k for k, v in canonical.items()}
 
 ALL = frozenset('abcdefg')
+
 
 def parse_row(line):
   segments, display = [x.strip().split() for x in line.split('|')]
@@ -23,11 +24,13 @@ def parse_row(line):
   assert len(display) == 4
   return segments, display
 
+
 def is_digit_consistent(digit, value):
   if len(digit) != len(canonical[value]):
     return False
   determined = set(d for d in digit if d.islower())
   return determined.issubset(canonical[value])
+
 
 def is_consistent(segments, mapping):
   for segment in segments:
@@ -36,25 +39,40 @@ def is_consistent(segments, mapping):
       return False
   return True
 
-def _make_mapping(segments, mapping):
+
+def generate_assignment_order(segments):
+  order = ''
+  for segment in sorted(segments, key=lambda s: (len(s), s)):
+    for s in segment:
+      if s not in order:
+        order += s
+  return order
+
+
+def _make_mapping(segments, order, mapping):
   if len(mapping) == 7:
     yield mapping
   else:
-    for f in ALL - set(mapping.keys()):
-      for t in ALL - set(mapping.values()):
-        temp = {**mapping, **{f:t}}
-        if is_consistent(segments, temp):
-          yield from _make_mapping(segments, temp)
+    f = order[0]
+    for t in ALL - set(mapping.values()):
+      temp = {**mapping, **{f: t}}
+      if is_consistent(segments, temp):
+        yield from _make_mapping(segments, order[1:], temp)
+
 
 def make_mapping(segments):
-  return next(_make_mapping(segments, {}))
+  return next(_make_mapping(segments, generate_assignment_order(segments), {}))
 
-inputs = [ parse_row(line) for line in sys.stdin]
+
+inputs = [parse_row(line) for line in sys.stdin]
 
 tot = 0
 for segments, display in inputs:
   mapping = make_mapping(segments)
-  s =sum([10**i * canonical_inv[frozenset(mapping[c] for c in d)] for i,d in zip((3,2,1,0),display)])
+  s = sum([
+      10**i * canonical_inv[frozenset(mapping[c] for c in d)]
+      for i, d in zip((3, 2, 1, 0), display)
+  ])
   tot += s
 
 print(tot)
