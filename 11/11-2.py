83df524d41147d14f80fc6fcfb4039fb877a9794
tobiasjs <tobiasjs@google.com>
2021-12-11 08:00:43 +0000

    Day 11

---
+++ 11/11-2.py
@@ -0,0 +1,31 @@
+import sys
+import numpy as np
+import itertools
+
+mtx = np.array([[int(x) for x in line.strip()] for line in sys.stdin])
+
+def flash(mtx):
+    count = 0
+    while True:
+        c = 0
+        for i in range(mtx.shape[0]):
+            for j in range(mtx.shape[1]):
+                il, jl = max(i-1,0), max(j-1,0)
+                ih, jh = min(i+2, mtx.shape[0]), min(j+2, mtx.shape[1])
+                if mtx[i,j] >= 10:
+                    mtx[il:ih, jl:jh] += 1
+                    mtx[i,j] = -100
+                    c += 1
+        count += c
+        if not c:
+            break
+    return count
+
+count = 0
+for i in itertools.count(1):
+    mtx += 1
+    count = flash(mtx)
+    if count == mtx.shape[0] * mtx.shape[1]:
+        print(i)
+        break
+    mtx[mtx<0] = 0
