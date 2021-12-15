daddd570db46807b26ce38fdfb36def412584dd7
tobiasjs <tobiasjs@google.com>
2021-12-15 14:14:04 +0000

    Day 15: add numba JITing and solution timing

--- 15/15.py
+++ 15/15.py
@@ -1,34 +1,42 @@
 import sys
 import numpy as np
 import heapq
-import itertools
+import numba
 
 mtx = np.array([[int(x) for x in line.strip()] for line in sys.stdin]).T
 
 
+@numba.njit
 def solve(mtx, repeats):
   pos = [(0, (0, 0))]
-  visited = {(0, 0)}
-
-  def neighbours(x, y, visited):
-    if x > 0 and (x - 1, y) not in visited: yield (x - 1, y)
-    if y > 0 and (x, y - 1) not in visited: yield (x, y - 1)
-    if x < mtx.shape[0] * repeats - 1 and (x + 1, y) not in visited:
-      yield (x + 1, y)
-    if y < mtx.shape[1] * repeats - 1 and (x, y + 1) not in visited:
-      yield (x, y + 1)
+  shape = np.array(mtx.shape) * repeats
+  visited = np.zeros((shape[0], shape[1]), dtype=np.bool_)
+  visited[0, 0] = True
 
   while True:
     score, (x, y) = heapq.heappop(pos)
-    if x == mtx.shape[0] * repeats - 1 and y == mtx.shape[1] * repeats - 1:
+    if x == shape[0] - 1 and y == shape[1] - 1:
       return score
-    for x2, y2 in neighbours(x, y, visited):
+    for x2, y2 in ((x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)):
+      if visited[max(0, min(x2, shape[0] - 1)), max(0, min(y2, shape[1] - 1))]:
+        continue
       xd, xm = divmod(x2, mtx.shape[0])
       yd, ym = divmod(y2, mtx.shape[1])
       s = (mtx[xm, ym] + xd + yd - 1) % 9 + 1
       heapq.heappush(pos, (score + s, (x2, y2)))
-      visited.add((x2, y2))
+      visited[x2, y2] = True
 
 
-print(solve(mtx, 1))
-print(solve(mtx, 5))
+TIMING = False
+
+import time
+for i in (1, 5):
+  print(solve(mtx, i))
+  if TIMING:
+    t = []
+    for times in range(100):
+      a = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+      solve(mtx, i)
+      b = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+      t.append(b - a)
+    print('median time (ms): ', np.median(np.array(t)) / 1e6)
